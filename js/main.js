c2857dac2cbd6119d3327f22689f90d65ad8e405
lucasweiby <lucas_weiby@hotmail.com>
2020-03-09 13:01:36 -0300

    Events section created

--- js/main.js
+++ js/main.js
@@ -41,4 +41,25 @@ $(document).ready(function() {
         $('.navbar').toggleClass('hide-nav');
         $('body').toggleClass('overflow-hidden');
     })
+
+    $('.home-left').slick({
+        dots: false,
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        infinite: true,
+        speed: 500,
+        fade: true,
+        cssEase: 'linear'
+    });
+
+    $('.tab-pane-gallery').slick({
+        dots: false,
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        speed: 500,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+    });
 });
